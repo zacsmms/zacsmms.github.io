ed9ff7d7cce891322ca17c67a0ae43681ba55ec3
zacsmms <77255406+zacsmms@users.noreply.github.com>
2026-05-14 01:45:23 +0000

    Deploying to gh-pages from @ zacsmms/zacsmms.github.io@b78918a780e0172d6e2c8bf1c35af704f711afd1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Either download the CV by clicking the icon on the right or scroll through here.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
